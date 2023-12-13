e30e835b6c1cc77b12861bb9bb8d82f7a42e0ab4
Dimas Dharu Ramadhan <dimasddr7@gmail.com>
2023-12-13 16:52:50 +0700

    first commit

---
+++ src/SimpleKalkulator.php
@@ -0,0 +1,21 @@
+<?php
+ 
+namespace Dharuuu\Kalkulator;
+ 
+/**
+ * Basic Calculator.
+ * 
+ */
+class SimpleKalkulator
+{
+    /**
+     * Menjumlahkan semua data dalam sebuah array.
+     *
+     * @param array $data
+     * @return integer
+     */
+    public static function penjumlahan(array $data)
+    {
+        return array_sum($data);
+    }
+}
